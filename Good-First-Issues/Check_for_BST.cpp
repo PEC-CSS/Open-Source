077143ac4061596920cb442a326235da57cb82d9
sukaran <sukarankant765@gmail.com>
2021-10-02 13:57:31 +0530

    check for BST issue #37

---
+++ Good-First-Issues/Check_for_BST.cpp
@@ -0,0 +1,80 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+    public:
+        int data;
+        Node* left;
+        Node* right;
+    Node(int data) {
+        this->data = data;
+        left = right = NULL;
+    }
+};
+
+Node* createBinaryTree() {
+    int x;
+    cin>>x;
+    if(x==-1) {
+        return NULL;
+    }
+    Node* root = new Node(x);
+    root->left = createBinaryTree();
+    root->right = createBinaryTree();
+    return root;
+}
+
+bool helper(Node* root,int lower,int upper) {
+    if(root==NULL) {
+        return true;
+    }
+    bool check1 = root->data<= upper && root->data >= lower;
+    bool check2 = helper(root->left,lower,root->data-1);
+    bool check3 = helper(root->right,root->data,upper);
+
+    return check1 && check2 && check3;
+}
+
+bool isBST(Node* root) {
+    return helper(root,INT_MIN,INT_MAX);
+}
+
+void printTree(Node* root) {
+    if(root==NULL)
+        return ;
+    cout<<root->data<<" ";
+    printTree(root->left);
+    printTree(root->right);
+}
+
+int main()
+{
+    cout<<"Input tree\n";
+    cout<<"-1 indicates that you want to enter null\n";
+
+// input  tree
+    Node* tree = createBinaryTree();
+
+// printing tree
+    cout<<"PreOrder of original Tree: ";
+    printTree(tree);
+    cout<<"\n";
+
+    bool ans = isBST(tree);
+    if(ans) {
+        cout<<"Yes, its a BST";
+    } else {
+        cout<<"Not a BST";
+    }
+
+}
+
+
+// g++ Mirror_binary_tree.cpp -o Mirror_binary_tree
+// ./Mirror_binary_tree.exe
+
+// sample ip: 1 2 4 -1 -1 5 -1 -1 3 6 -1 -1 7 -1 -1
+// op :
+// PreOrder of original Tree: 1 2 4 5 3 6 7
+// PreOrder of Mirrored Tree: 1 3 7 6 2 5 4
