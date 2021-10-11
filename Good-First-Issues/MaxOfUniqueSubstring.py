9bc6b640d3bb4895922da67e61ccd54507cdefc6
vdeodataseer <87189258+vdeodataseer@users.noreply.github.com>
2021-10-12 03:09:42 +0530

    Code for max of unique substrings in python

--- Good-First-Issues/MaxOfUniqueSubstring.py
+++ Good-First-Issues/MaxOfUniqueSubstring.py
@@ -1,16 +1,16 @@
 class Solution:
     def maxUniqueSplit(self, s: str) -> int:
-        global cnt
-        cnt = 0
-        check = defaultdict(int)
+        global value_holder  #declared a global variable 
+        value_holder = 0 #initialized the same to 0
+        check = defaultdict(int) 
         def rec(index,s,ans):
-            global cnt
+            global value_holder
             if(index>=len(s)):
-                cnt = max(cnt,ans)
+                value_holder = max(value_holder,ans)
             for i in range(index+1,len(s)+1):
                 if(check[s[index:i]] == 0):
                     check[s[index:i]] = 1
                     rec(i,s,ans+1)
                     check[s[index:i]] = 0
         rec(0,s,0)
-        return cnt
+        return value_holder
