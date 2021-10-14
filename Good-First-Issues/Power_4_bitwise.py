3a3599ca698cb86dc4e0832ebcf43e7cbd0558d5
prashantkumarshee@gmail.com <prashantkumarpd@gmail.com>
2021-10-14 18:44:05 +0530

    Power of Four #297

--- Good-First-Issues/Power_4_bitwise.py
+++ Good-First-Issues/Power_4_bitwise.py
@@ -1,13 +1,21 @@
 # function for check power of 4 by Using bit manipulation.
 # N=integer input
-def hacktoberfest(N):
-    if (N == 0):
-        return False
-    while (N != 1):
-            if (N % 4 != 0):
+# leetcode-342
+class Solution:
+    def isPowerOfFour(self, num: int) -> bool:
+        
+        if num < 0:
+            return False
+
+        s = bin(num).replace("0b", "")
+        # number power of 4 has total odd number of bits with only '1' bit
+
+        if s.count("1") == 1:
+            if len(s) % 2 == 1:
+                return True
+            else:
                 return False
-            N = N // 4
-             
-    return True
+        else:
+            return False
 
 # Auther: Prashant kumar
