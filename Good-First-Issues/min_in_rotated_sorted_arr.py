7a99e19509e624137ceda2478efd6f6ee9c052a1
coderGtm <66418526+coderGtm@users.noreply.github.com>
2021-10-23 15:52:18 +0530

    Update min_in_rotated_sorted_arr.py
    reduced time complexity to O(log n)

--- Good-First-Issues/min_in_rotated_sorted_arr.py
+++ Good-First-Issues/min_in_rotated_sorted_arr.py
@@ -1,3 +1,20 @@
-array = [3,4,5,1,2]
+#using binary search approch to have O(log n) complexity
 
-print(min(array))
+def findMin(arr, low, high):
+    if high < low:
+        return arr[0]
+ 
+    if high == low:
+        return arr[low]
+ 
+    mid = int((low + high)/2)
+ 
+    if mid < high and arr[mid+1] < arr[mid]:
+        return arr[mid+1]
+ 
+    if mid > low and arr[mid] < arr[mid - 1]:
+        return arr[mid]
+ 
+    if arr[high] > arr[mid]:
+        return findMin(arr, low, mid-1)
+    return findMin(arr, mid+1, high)
